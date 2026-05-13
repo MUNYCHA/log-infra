f18bc215246af877a6f7aaaaf9c820993b25c8e2
munycha <munycha2004@gmail.com>
2026-05-13 10:16:08 +0700

    feat(auth): add Keycloak SSO, update Nginx and deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,21 @@ git -C ../log-dashboard-ui pull
 echo "Pulling latest log-infra..."
 git pull
 
-echo "Loading React env..."
+echo "Loading env..."
 source ../log-dashboard-ui/.env
+source .env
 
 echo "Building and starting containers..."
 VITE_WS_URL=$VITE_WS_URL \
 VITE_MAX_LOGS_PER_TOPIC=$VITE_MAX_LOGS_PER_TOPIC \
 VITE_MAX_MESSAGE_LENGTH=$VITE_MAX_MESSAGE_LENGTH \
+VITE_SSO_LOGIN_URL=$VITE_SSO_LOGIN_URL \
+VITE_SSO_LOGOUT_URL=$VITE_SSO_LOGOUT_URL \
+VITE_SSO_TOKEN_URL=$VITE_SSO_TOKEN_URL \
+VITE_SSO_CLIENT_ID=$VITE_SSO_CLIENT_ID \
+KC_ADMIN=$KC_ADMIN \
+KC_ADMIN_PASSWORD=$KC_ADMIN_PASSWORD \
+KC_HOSTNAME_URL=$KC_HOSTNAME_URL \
 docker compose up --build -d
 
 echo "Done."
